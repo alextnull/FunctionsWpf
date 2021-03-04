0ea3331f151e31e3bee197847abf74f9b96b91af
alextnull <alexander.t.null@outlook.com>
2021-03-04 23:42:38 +0700

    Реализация интерфейса INotifyPropertyChanged в MainWindowViewModel

--- FunctionsWpf/ViewModels/MainWindowViewModel.cs
+++ FunctionsWpf/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace FunctionsWpf.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : INotifyPropertyChanged
     {
+        #region Реализация интерфейса INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Вызывает событие PropertyChanged при изменении свойста класса.
+        /// </summary>
+        /// <param name="property">Имя свойства.</param>
+        protected void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        /// <summary>
+        /// Присваивает по ссылке переменной field значение value.
+        /// </summary>
+        /// <typeparam name="T">Тип параметров field и value.</typeparam>
+        /// <param name="field">Ссылка на переменную.</param>
+        /// <param name="value">Значение, которое необходимо присвоить field.</param>
+        /// <param name="property">Имя свойства.</param>
+        /// <returns>Возвращает false, если значение field равно value, иначе true.</returns>
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string property = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(property);
+            return true;
+        }
+
+        #endregion
+
     }
 }
