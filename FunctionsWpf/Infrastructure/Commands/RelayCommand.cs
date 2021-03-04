31223ad3f62ade31d907de5bb8a753a59cb63148
alextnull <alexander.t.null@outlook.com>
2021-03-04 16:31:40 +0700

    Реализация класса RelayCommand для создания команд

---
+++ FunctionsWpf/Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FunctionsWpf.Infrastructure.Commands
+{
+    internal class RelayCommand : CommandBase
+    {
+        /// <summary>
+        /// Указывает на метод, который необходимо вызвать при выполнении команды. 
+        /// Параметр object представляет параметр команды.
+        /// </summary>
+        private readonly Action<object> _execute;
+
+        /// <summary>
+        /// Указывает на метод, который необходимо вызвать для проверки доступности выполнения команды. 
+        /// Параметр object представляет параметр команды./// </summary>
+        /// <returns>Возвращает true если команду можно выполнить, иначе false.</returns>
+        private readonly Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// Инициализирует внутренние делегаты переданными методами для выполнения команды и проверки доступности выполнения команды соответственно.
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Проверяет возможно ли выполнить команду.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns>Если команду можно выполнить или метод проверки не был передан в конструктор, тогда возвращает true, иначе false</returns>
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        /// <summary>
+        /// Выполняет команду.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
