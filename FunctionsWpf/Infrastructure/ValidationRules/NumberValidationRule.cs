412f59037e7e8db73c9ab68c17423fd03f62b980
alextnull <alexander.t.null@outlook.com>
2021-03-05 12:06:44 +0700

    Класс для верификации введённых значений

---
+++ FunctionsWpf/Infrastructure/ValidationRules/NumberValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace FunctionsWpf.Infrastructure.ValidationRules
+{
+    internal class NumberValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int number;
+            bool isValid = int.TryParse(value as string, out number);
+            return new ValidationResult(isValid, "Не допустимое значение");
+        }
+    }
+}
