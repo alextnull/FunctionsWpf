239ccb5f1aea4142ab69ab7fa2611033e9ff196b
alextnull <alexander.t.null@outlook.com>
2021-03-05 20:14:08 +0700

    Проект для Unit тестирования

---
+++ FunctionWpfTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunctionWpfTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
